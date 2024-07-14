a71db822b2ffd9c5f9d60bd50a1d030af4e242a2
egijago <13521056@mahasiswa.itb.ac.id>
2024-07-14 21:27:36 +0700

    feat: electron wrapper

--- app.js
+++ app.js
@@ -1,69 +1,39 @@
-const { app, BrowserWindow, protocol } = require("electron")
-const url = require("url")
-const path = require("path")
+const {app, BrowserWindow} = require('electron')
+    const url = require("url");
+    const path = require("path");
 
-let mainWindow
+    let mainWindow
 
-function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-      nodeIntegrationInWorker: true,
-      webSecurity: true,
-    },
-  })
+    function createWindow () {
+      mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+          nodeIntegration: true
+        }
+      })
 
-  mainWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, `/dist/olmap/index.html`),
-      protocol: "file:",
-      slashes: true,
-    }),
-  )
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools()
-
-  mainWindow.on("closed", function () {
-    mainWindow = null
-  })
-  mainWindow.webContents.on("did-fail-load", () =>
-    {
-      console.error(`Failed to load URL: ${event.errorDescription}`)
       mainWindow.loadURL(
         url.format({
           pathname: path.join(__dirname, `/dist/olmap/index.html`),
           protocol: "file:",
-          slashes: true,
-        }),
-      )
+          slashes: true
+        })
+      );
+      // Open the DevTools.
+      mainWindow.webContents.openDevTools()
+
+      mainWindow.on('closed', function () {
+        mainWindow = null
+      })
     }
-  )
 
-  // protocol.registerFileProtocol('file', (request, callback) => {
-  //   const filePath = request.url.replace('file:///', '');
-  //   const normalizedPath = path.normalize(`${__dirname}/${filePath}`);
-  //   callback(normalizedPath);
-  // });
+    app.on('ready', createWindow)
 
-  // protocol.interceptFileProtocol('file', (request, callback) => {
-  //   const url = request.url.substr(7); // Remove the 'file://' prefix
-  //   callback({ path: path.normalize(`${__dirname}/${url}`) });
-  // });
-}
+    app.on('window-all-closed', function () {
+      if (process.platform !== 'darwin') app.quit()
+    })
 
-app.on("ready", createWindow)
-
-app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") app.quit()
-})
-
-app.on("will-prevent-unload", (event) => {
-  // Handle the event to prevent the default behavior, if necessary
-  event.preventDefault()
-})
-
-app.on("activate", function () {
-  if (mainWindow === null) createWindow()
-})
+    app.on('activate', function () {
+      if (mainWindow === null) createWindow()
+    })
